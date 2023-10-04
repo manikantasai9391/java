62e897aea86e9732c023a984fadec9eff46af216
manikantasai9391 <142982153+manikantasai9391@users.noreply.github.com>
2023-10-04 09:42:19 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class Animal
+{
+    void eat()
+    {
+        System.out.println(" Animal can eat ");
+    }
+}
+class Dog extends Animal
+{
+    void bark()
+    {
+        System.out.println(" Dog can bark ");
+    }
+}
+public class singleInheritanceDemo
+{
+    public static void main(String args[])
+    {
+        Dog d = new Dog();
+        d.bark();
+        d.eat();
+    }
+}
